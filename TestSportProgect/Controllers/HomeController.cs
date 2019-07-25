d7d35f11a758436597b6bbf80863c4625d852de4
radk510 <radk510@yandex.ru>
2019-07-25 23:25:34 +0300

    feat: add logic to make ajax view

--- TestSportProgect/Controllers/HomeController.cs
+++ TestSportProgect/Controllers/HomeController.cs
@@ -12,16 +12,30 @@ namespace TestSportProgect.Controllers
     {
         ClubContext database = new ClubContext();
         public ActionResult Index()
-        {
-            
+        {           
             return View();
         }
 
         public ActionResult FootballClubs()
         {
             return View();
+        }        
+        public ActionResult EnglandClubsList()
+        {
+            var clubs = database.Clubs.Where(i => i.League.Name == "FA Premier League").ToList();
+            return PartialView(clubs);
         }
 
-       
+        public ActionResult SpainClubsList()
+        {
+            var clubs = database.Clubs.Where(i => i.League.Name == "LA Liga").ToList();
+            return PartialView(clubs);
+        }
+
+        public ActionResult ItalyClubsList()
+        {
+            var clubs = database.Clubs.Where(i => i.League.Name == "Seria A").ToList();
+            return PartialView(clubs);
+        }
     }
 }
